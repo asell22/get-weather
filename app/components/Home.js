f4cd5ead6418f071407edbce93a7e29eb43ce766
asell22 <alexanderjsell@gmail.com>
2016-10-05 19:33:07 -0700

    style for header in Home component

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,12 +1,16 @@
 var React = require('react');
-var background = require('../styles').background;
+var styles = require('../styles');
+var background = styles.background;
+var content = styles.content;
+var center = styles.header
 
 function Home() {
   return (
     <div style={background}>
-      <div className="container">
-        <h2>Enter a City and State</h2>
+      <div className="container" style={content}>
+        <h2 style={center}>Enter a City and State</h2>
         <input type="text" className="form-control" placeholder="Petaluma, CA"/>
+        <button className="btn btn-success">Get Weather</button>
       </div>
     </div>
   )
--- app/styles/index.js
+++ app/styles/index.js
@@ -2,6 +2,16 @@ var styles = {
   background: {
     background: '#4cf1a3',
     height: '100vh'
+  },
+  content: {
+    paddingTop: '100px',
+    // background: 'red'
+  },
+  header: {
+    textAlign: 'center',
+    fontSize: '50px',
+    color: '#fff',
+    fontWeight: '100'
   }
 }
 
