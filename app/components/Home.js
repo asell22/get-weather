79de00cb49b22cdcba45f29d7f4f96ad7d7af615
asell22 <alexanderjsell@gmail.com>
2016-10-07 10:48:07 -0700

    Change Home and Nav components to container components

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,23 +1,38 @@
 var React = require('react');
 var merge = require('lodash/merge')
 var styles = require('../styles');
-var background = styles.background;
-var content = styles.content;
-var header = styles.header;
-var input = styles.input;
-var center = styles.center;
-var button = styles.button
 
-function Home() {
-  return (
-    <div style={background}>
-      <div className="container" style={content}>
-        <h2 style={header}>Enter a City and State</h2>
-        <input type="text" className="form-control" style={merge({}, input, center)} placeholder="Petaluma, CA"/>
-        <button className="btn btn-success"  style={merge({}, button, center)}>Get Weather</button>
+var background = styles.background,
+    content = styles.content,
+    header = styles.header,
+    input = styles.input,
+    center = styles.center,
+    button = styles.button;
+
+// function Home() {
+//   return (
+//     <div style={background}>
+//       <div className="container" style={content}>
+//         <h2 style={header}>Enter a City and State</h2>
+//         <input type="text" className="form-control" style={merge({}, input, center)} placeholder="Petaluma, CA"/>
+//         <button className="btn btn-success"  style={merge({}, button, center)}>Get Weather</button>
+//       </div>
+//     </div>
+//   )
+// }
+
+var Home = React.createClass({
+  render: function() {
+    return (
+      <div style={background}>
+        <div className="container" style={content}>
+          <h2 style={header}>Enter a City and State</h2>
+          <input type="text" className="form-control" style={merge({}, input, center)} placeholder="Petaluma, CA"/>
+          <button className="btn btn-success"  style={merge({}, button, center)}>Get Weather</button>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+})
 
 module.exports = Home;
--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,27 +1,52 @@
 var React = require('react');
-var nav = require('../styles/Nav').nav;
-var color = require('../styles/Nav').color
-var borderColor = require('../styles/Nav').borderColor
-var margin = require('../styles/Nav').margin
+var navStyles = require('../styles/Nav');
 
-function Nav() {
-  return (
-    <nav className="navbar navbar-default navbar-fixed-top" style={nav}>
-     <div className="container">
-       <div className="navbar-header">
-          <a className="navbar-brand" href="#" style={color}>Get Weather</a>
-       </div>
-       <div id="navbar" className="navbar-collapse collapse">
-          <form className="navbar-form navbar-right">
-            <div className="form-group">
-              <input type="text" className="form-control" placeholder="Petaluma, CA" style={margin}/>
-            </div>
-            <button type="submit" className="btn btn-default">Get Weather</button>
-          </form>
-       </div>
-     </div>
-   </nav>
-  )
-}
+var nav = navStyles.nav,
+    color = navStyles.color,
+    borderColor = navStyles.borderColor,
+    margin = navStyles.margin;
+
+// function Nav() {
+//
+//   return (
+//     <nav className="navbar navbar-default navbar-fixed-top" style={nav}>
+//      <div className="container">
+//        <div className="navbar-header">
+//           <a className="navbar-brand" href="#" style={color}>Get Weather</a>
+//        </div>
+//        <div id="navbar" className="navbar-collapse collapse">
+//           <form className="navbar-form navbar-right">
+//             <div className="form-group">
+//               <input type="text" className="form-control" placeholder="Petaluma, CA" style={margin}/>
+//             </div>
+//             <button type="submit" className="btn btn-default">Get Weather</button>
+//           </form>
+//        </div>
+//      </div>
+//    </nav>
+//   )
+// }
+
+var Nav = React.createClass({
+  render: function() {
+    return (
+      <nav className="navbar navbar-default navbar-fixed-top" style={nav}>
+        <div className="container">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#" style={color}>Get Weather</a>
+          </div>
+          <div id="navbar" className="navbar-collapse collapse">
+            <form className="navbar-form navbar-right">
+              <div className="form-group">
+                <input type="text" className="form-control" placeholder="Petaluma, CA" style={margin}/>
+              </div>
+              <button type="submit" className="btn btn-default">Get Weather</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+})
 
 module.exports = Nav;
