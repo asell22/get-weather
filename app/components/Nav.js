69de516873f2dbac7b8c3bda30e61c6aaa96a6b7
asell22 <alexanderjsell@gmail.com>
2016-10-07 11:26:25 -0700

    Log out nav component's state on form submit

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -7,6 +7,22 @@ var nav = navStyles.nav,
     margin = navStyles.margin;
 
 var Nav = React.createClass({
+  getInitialState: function() {
+    return {
+      place: ''
+    }
+  },
+  handleUpdatePlace: function(evt) {
+    this.setState({
+      place: evt.target.value
+    })
+  },
+  handleFormSubmit: function() {
+    console.log(this.state.place);
+    this.setState({
+      place: ''
+    })
+  },
   render: function() {
     return (
       <nav className="navbar navbar-default navbar-fixed-top" style={nav}>
@@ -15,9 +31,19 @@ var Nav = React.createClass({
             <a className="navbar-brand" href="#" style={color}>Get Weather</a>
           </div>
           <div id="navbar" className="navbar-collapse collapse">
-            <form className="navbar-form navbar-right">
+            <form
+              className="navbar-form navbar-right"
+              onSubmit={this.handleFormSubmit}
+            >
               <div className="form-group">
-                <input type="text" className="form-control" placeholder="Petaluma, CA" style={margin}/>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Petaluma, CA"
+                  style={margin}
+                  onChange={this.handleUpdatePlace}
+                  value={this.state.place}
+                  />
               </div>
               <button type="submit" className="btn btn-default">Get Weather</button>
             </form>
