aedb5ff25d64e61588a4bd67cd2f993f7b3c5364
asell22 <alexanderjsell@gmail.com>
2016-10-07 14:28:17 -0700

    place nav container ui into stateless functional component

---
+++ app/components/Nav.js
@@ -0,0 +1,39 @@
+var React = require('react');
+var navStyles = require('../styles/Nav');
+
+var nav = navStyles.nav,
+    color = navStyles.color,
+    borderColor = navStyles.borderColor,
+    margin = navStyles.margin;
+
+var Nav = function(props) {
+  return (
+    <nav className="navbar navbar-default navbar-fixed-top" style={nav}>
+      <div className="container">
+        <div className="navbar-header">
+          <a className="navbar-brand" href="#" style={color}>Get Weather</a>
+        </div>
+        <div id="navbar" className="navbar-collapse collapse">
+          <form
+            className="navbar-form navbar-right"
+            onSubmit={props.onFormSubmit}
+          >
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Petaluma, CA"
+                style={margin}
+                onChange={props.onUpdatePlace}
+                value={props.place}
+                />
+            </div>
+            <button type="submit" className="btn btn-default">Get Weather</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+module.exports = Nav;
--- app/containers/NavContainer.js
+++ app/containers/NavContainer.js
@@ -1,10 +1,5 @@
 var React = require('react');
-var navStyles = require('../styles/Nav');
-
-var nav = navStyles.nav,
-    color = navStyles.color,
-    borderColor = navStyles.borderColor,
-    margin = navStyles.margin;
+var Nav = require('../components/Nav');
 
 var NavContainer = React.createClass({
   getInitialState: function() {
@@ -25,32 +20,39 @@ var NavContainer = React.createClass({
     })
   },
   render: function() {
+    // return (
+    //   <nav className="navbar navbar-default navbar-fixed-top" style={nav}>
+    //     <div className="container">
+    //       <div className="navbar-header">
+    //         <a className="navbar-brand" href="#" style={color}>Get Weather</a>
+    //       </div>
+    //       <div id="navbar" className="navbar-collapse collapse">
+    //         <form
+    //           className="navbar-form navbar-right"
+    //           onSubmit={this.handleFormSubmit}
+    //         >
+    //           <div className="form-group">
+    //             <input
+    //               type="text"
+    //               className="form-control"
+    //               placeholder="Petaluma, CA"
+    //               style={margin}
+    //               onChange={this.handleUpdatePlace}
+    //               value={this.state.place}
+    //               />
+    //           </div>
+    //           <button type="submit" className="btn btn-default">Get Weather</button>
+    //         </form>
+    //       </div>
+    //     </div>
+    //   </nav>
+    // )
     return (
-      <nav className="navbar navbar-default navbar-fixed-top" style={nav}>
-        <div className="container">
-          <div className="navbar-header">
-            <a className="navbar-brand" href="#" style={color}>Get Weather</a>
-          </div>
-          <div id="navbar" className="navbar-collapse collapse">
-            <form
-              className="navbar-form navbar-right"
-              onSubmit={this.handleFormSubmit}
-            >
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Petaluma, CA"
-                  style={margin}
-                  onChange={this.handleUpdatePlace}
-                  value={this.state.place}
-                  />
-              </div>
-              <button type="submit" className="btn btn-default">Get Weather</button>
-            </form>
-          </div>
-        </div>
-      </nav>
+      <Nav
+        onFormSubmit={this.handleFormSubmit}
+        onUpdatePlace={this.handleUpdatePlace}
+        place={this.state.place}
+      />
     )
   }
 })
