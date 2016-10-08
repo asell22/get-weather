c7cbfaa5af57d6ddd6148f3adce8d53501d2e13e
asell22 <alexanderjsell@gmail.com>
2016-10-08 11:39:36 -0700

    API request for Fahrenheit temp

--- app/utils/weatherHelpers.js
+++ app/utils/weatherHelpers.js
@@ -4,7 +4,7 @@ var API_Key = require('../../key');
 var helpers = {
   getCurrentWeather: function (location) {
     // fetch data from OpenWeather Api
-    return axios.get('http://api.openweathermap.org/data/2.5/weather?q='+location+'&type=accurate&APPID='+API_Key).then(function(data) {
+    return axios.get('http://api.openweathermap.org/data/2.5/weather?q='+location+'&type=accurate&units=imperial&APPID='+API_Key).then(function(data) {
       console.log('DATA:', data);
     })
   }
