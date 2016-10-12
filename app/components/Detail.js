a7772fefd223c1c697dcbc567dc7fefd26f7e532
asell22 <alexanderjsell@gmail.com>
2016-10-12 15:58:48 -0700

    Create Detail Component

---
+++ app/components/Detail.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+function Detail(props) {
+  return (
+    <h1 style={{paddingTop: '100px'}}>{props.type}</h1>
+  )
+}
+
+module.exports = Detail
--- app/containers/DetailContainer.js
+++ app/containers/DetailContainer.js
@@ -1,13 +1,12 @@
 var React = require('react');
+var Detail = require('../components/Detail');
 
 var DetailContainer = React.createClass({
   render: function() {
     var details = this.props.location.state.details;
-    console.log(details);
+    
     return (
-      <div style={{paddingTop: '100px'}}>
-        <h1>DETAIL</h1>
-      </div>
+      <Detail type={details.type}/>
     )
   }
 })
